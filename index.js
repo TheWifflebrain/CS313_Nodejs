a2d842255e0c6df98ac1fa9f5ed8d5b9ffb8d74b
TheWifflebrain <blackice150@gmail.com>
2019-06-23 15:41:29 -0600

    connecting17

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-var app = express();
+const app = express();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/home', (req, res) => res.render('public/index'))
+  .get('/', (req, res) => res.render('public/index'))
   .get('/getInfo', getInfo)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
