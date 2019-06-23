09bb75957cc79ecc1b2a49ac4e6f07701e72fb8d
TheWifflebrain <blackice150@gmail.com>
2019-06-23 14:42:30 -0600

    connecting14

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('public/home'))
+  .get('/home', (req, res) => res.render('public/home'))
+  
   .get('/getInfo', getInfo)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
