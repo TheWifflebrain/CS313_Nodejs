ff70d078be23a4b846592c0d7ebe5931ebecf550
TheWifflebrain <blackice150@gmail.com>
2019-06-23 12:35:56 -0600

    connecting3

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function sLetter(weight){
       break
   }
   var params = {package: package, weight: weight, price: price};
-	res.render("cost", params);
+	res.render("../views/cost", params);
 }
 
 function mLetter(weight){
